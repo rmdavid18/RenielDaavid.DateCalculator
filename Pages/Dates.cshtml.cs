8d4d77205259bf237c56c1eb4fd087021c831158
janwe <janwe@LAPTOP-FLN6CH61>
2022-10-06 18:13:11 +0800

    Add project files.

---
+++ Pages/Dates.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RenielDaavid.DateCalculator.Pages
+{
+    public class DatesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
